7de59c4f607aefe9259c29f38ea2af67e8ce7ea2
Mahesh Sutar <71620737+MaheshS-ctrl@users.noreply.github.com>
2025-05-20 16:22:36 +0000

    commit

--- Contact-Page/src/Components/ContactForm.jsx
+++ Contact-Page/src/Components/ContactForm.jsx
@@ -1,22 +1,46 @@
 import React from 'react'
+import { useState } from 'react'
 
 
 
 const ContactForm = () => {
+
+    const [name,setName] = useState("");
+    const [email,setEmail] = useState("");
+    const [text,setText] = useState("");
+
+const onviachatclick = () => {
+    console.log("Hello im From Chat")
+}
+
+const onviacallclick = () => {
+    console.log("Hello im From Call")
+}
+
+const onSubmit = (event) => {
+
+    event.preventDefault();
+
+    setName(event.target[1].value);
+    setEmail(event.target[3].value);
+    setText(event.target[5].value)
+
+}
+
     return (
         <div className='parent'>
 
             <div className="twoparts">
 
                 <div className='button'>
-                    <button id='chat'>VIA SUPPORT CHAT</button>
-                    <button id='call'>VIA CALL</button>
+                    <button onClick={onviachatclick} id='chat'>VIA SUPPORT CHAT</button>
+                    <button onClick={onviacallclick} id='call'>VIA CALL</button>
                 </div>
                 <br />
                 <button className='emailform'>VIA EMAIL FORM</button>
                 <br />
 
-                <form className='form'>
+                <form className='form' onSubmit={onSubmit}>
                     <br />
                     <fieldset>
                         <legend>Name</legend>
@@ -47,6 +71,13 @@ const ContactForm = () => {
 
             <div className='services1'>
                 <img src="./images/Service.svg" alt="" />
+            </div>
+
+            <div>
+                Data From FORM :
+                <p>{name}</p>
+                <p>{email}</p>
+                <p>{text}</p>
             </div>
 
 
